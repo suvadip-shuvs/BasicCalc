7f5f5c4f71bd17ee120d1eb0a9b71402c13e6d93
sjallal <sjallal88@gmail.com>
2020-06-06 17:25:57 +0530

    Added the add feature in js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function add(){
     //Write the code to add Saqib
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = num1 + num2;
 }
 
 function subtract(){
