0bf7d5835444b64b022985897d66ffdb21c81001
pragati-k <kundupragati2@gmail.com>
2020-06-06 17:48:15 +0530

    added multiply function

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function subtract(){
 }
 
 //Supriyo & Pragati please add function like divide and multiplication
+function multiply(){
+    var num1= parseInt(document.getElementById("num1").value);
+    var num2= parseInt(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = num1*num2;
+}
