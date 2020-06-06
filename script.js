5db2be7fd8dd9dd3414bfd15109119a9cdf19772
suvadip-shuvs <suvadip.duke@gmail.com>
2020-06-06 17:01:58 +0530

    Create the html and assigned the work in css & js

---
+++ script.js
@@ -0,0 +1,9 @@
+function add(){
+    //Write the code to add Saqib
+}
+
+function subtract(){
+    //Write the code to subtract Moitreeye
+}
+
+//Supriyo & Pragati please add function like divide and multiplication
